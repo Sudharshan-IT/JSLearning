6b784f6c47672cce67f039baf5c0849f16dac2bc
Sudharshan <sudharshanjavaguide@gmail.com>
2023-07-23 16:28:16 +0530

    Added movieList Function

--- SpreadOperator.js
+++ SpreadOperator.js
@@ -5,3 +5,11 @@ test(120);
 test("Sudharshan","Nandyala","Yadav");
 test("300.0",null,"Nandyala");
 test(undefined,null,undefined);
+
+function movieList(...list){
+console.log(list);
+}
+console.log("RRR","Devara","Temper");
+
+
+
