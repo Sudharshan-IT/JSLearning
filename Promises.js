72d94fed4f42fb16076af027beff6944c19498ea
Sudharshan <sudharshanjavaguide@gmail.com>
2023-08-23 08:48:00 +0530

    Added Few Functions

---
+++ Promises.js
@@ -0,0 +1,15 @@
+let welcome=(user)=>{
+    const name="Sudharshan";
+    if(name.startsWith("S")){
+
+    }else{
+        
+    }
+
+}
+welcome(["Sudharshan","Nandyala","Yadav"]);
+let add=(a,b)=>{
+    let c=a+b;
+    console.log(c);
+}
+add(10,20);
