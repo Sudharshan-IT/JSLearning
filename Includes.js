cbe24c3faf88c5ef957b61bb78586aa9bb9da657
Sudharshan <sudharshanjavaguide@gmail.com>
2023-08-25 14:15:21 +0530

    Added Includes File

---
+++ Includes.js
@@ -0,0 +1,29 @@
+let names=["Chennai","Hyderabad","Bangalore","Keral","Karnataka","Vijayawada"];
+let findingCity=()=>{
+    /*
+if(names.includes("Chennai")){
+    return "Chennai Is Part Of Names Array";
+}else{
+    return 'Chennai Is not part of Names Array';
+}
+*/
+let isTure=names.includes("Hyderabad")?" Yes":"NO";
+console.log(isTure);
+}
+console.log(findingCity());
+
+let age=45;
+/*
+age>30 ? age>70 ?
+console.log("Your are Getting Old"):
+console.log("you are between 30 & 69"):
+console.log("You are below 10");
+*/
+let findResult=(Marks)=>{
+let results=(Marks<40)?"UnSatisfied":
+            (Marks<60)?"Average":
+            (Marks<80)?"Good":"Excellent";
+            return results;
+}
+console.log(findResult(50));
+    
